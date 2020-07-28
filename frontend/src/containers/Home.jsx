def0555330065ce74882f7dc3e5f81aa7afd446c
MukkuMayc <st063775@student.spbu.ru>
2020-07-28 23:44:45 +0300

    Change messages whether request is ok or not

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -5,21 +5,32 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      message: "Loading...",
+      loading: true,
     };
   }
 
   componentDidMount() {
     console.log(config.serverUrl + "/hello");
     fetch(config.serverUrl + "/hello")
-      .then((res) => res.text())
-      .then((res) => this.setState({ message: res }));
+      .then((res) => {
+        res.text().then((text) => {
+          this.setState({ loading: false, ok: res.ok, text: text });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
   }
 
   render() {
     return (
-      <div>
-        <p>{this.state.message}</p>
+      <div className="Home">
+        {this.state.loading
+          ? "Loading..."
+          : this.state.ok && this.state.text
+          ? "Congratulations!"
+          : "Something went wrong"}
       </div>
     );
   }
