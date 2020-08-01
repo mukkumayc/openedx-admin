813bb846a0baf4fc4d82cc87c8459ab431708810
MukkuMayc <st063775@student.spbu.ru>
2020-08-01 03:16:42 +0300

    Add navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Routes from "./components/Routes";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes />
     </div>
   );
