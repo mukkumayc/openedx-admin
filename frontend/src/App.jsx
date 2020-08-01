129bf4af298e2c775ca8f828e1a42c6143a788f2
MukkuMayc <st063775@student.spbu.ru>
2020-08-02 02:56:18 +0300

    Add authentication check at the start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./components/Routes";
 import NavBar from "./components/NavBar";
+import Config from "./config";
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setAuthenticating] = useState(false);
+
+  useEffect(() => {
+    setAuthenticating(true);
+    console.log(document.cookie);
+    fetch(Config.serverUrl + "/checktoken", {
+      credentials: "include",
+    })
+      .then((res) => {
+        userHasAuthenticated(res.ok);
+        setAuthenticating(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setAuthenticating(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <NavBar />
-      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
-    </div>
+    !isAuthenticating && (
+      <div className="App">
+        <NavBar />
+        <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+      </div>
+    )
   );
 }
 
