0edf4bfcdd4117bd3808531f6e9389b76ab8e78f
mukkumayc <sk9078@ya.ru>
2022-11-12 11:58:27 +0600

    add ./src dir as @ to resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	},
 	css: {
 		devSourcemap: true
 	},
